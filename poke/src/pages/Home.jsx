69cd15d88cddaef46dc1dbc65ea5f4c4d5308fee
David Valencia <codingdavid9@outlook.com>
2021-06-18 09:56:51 -0500

    API consumida, ahora traer a todos los pokemons

--- poke/src/pages/Home.jsx
+++ poke/src/pages/Home.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
 
+import PokemonHome from '../components/PokemonHome'
+
 class Home extends React.Component {
-  state = {
-    data: {
-      results:[{
-        url:[]
-      }]
-    },
-  }
-
-  componentDidMount(){
-    this.fetchCharacters()
-  }
-
-  fetchCharacters = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
-    const data = await response.json()
-
-    this.setState({
-      data: data,
-    })
-  }
-
   render() {
     return (
-      <div className="Characters">
-        {/* {console.log(this.state.stats)} */}
-        <ul className="Characters-item">
-          {this.state.data.results.map(character => (
-            <li>{character.name}</li>
-          ))}
-        </ul>
-      </div>
+    <>
+      <PokemonHome/>
+    </>
     );
   }
 }
